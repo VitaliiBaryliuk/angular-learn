477b661a30efc71e54fdb0ba60e90ad6bd23b7ed
Vitaly Baryliuk <v.baryliuk@devcaz.com>
2020-11-12 23:36:39 +0200

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+
+//@ts-ignore
+export class AppComponent {
+    name= '';
+}
